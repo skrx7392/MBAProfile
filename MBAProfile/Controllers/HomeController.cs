437550436f971a9d6645e26b5d9622aa1eca33be
Sekhar <Sekhar@DESKTOP-A0558KS>
2016-11-13 12:18:55 -0500

    Revert "Revert "Added comments""
    This reverts commit 34597291cc8a9d52fe7638e38a087b8e16147c3f.

--- MBAProfile/Controllers/HomeController.cs
+++ MBAProfile/Controllers/HomeController.cs
@@ -9,8 +9,14 @@ using System.Web.Script.Serialization;
 
 namespace MBAProfile.Controllers
 {
+
     public class HomeController : ApiController
     {
+        /// <summary>
+        /// Authenticates user from Database
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         [Route("Login")]
         [HttpPost]
         public IHttpActionResult ValidateUser([FromBody]UserLogin user)
@@ -27,6 +33,10 @@ namespace MBAProfile.Controllers
         }
 
         #region HttpGet Methods
+        /// <summary>
+        /// Get All Student Details
+        /// </summary>
+        /// <returns></returns>
         [Route("getStudents")]
         public IHttpActionResult getUCMStudentDetails()
         {
@@ -36,6 +46,11 @@ namespace MBAProfile.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves user with his Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [Route("getUser/{id}")]
         public IHttpActionResult getUCMUser(int id)
         {
@@ -43,6 +58,7 @@ namespace MBAProfile.Controllers
             return Ok(unitOfWork.UserInfo.GetUser(id));
         }
 
+        /// Pulls all the advisors list
         [Route("getAdvisors")]
         public IHttpActionResult getUCMAdvisors()
         {
@@ -52,15 +68,20 @@ namespace MBAProfile.Controllers
             }
         }
 
+        /// Pulls Director Details
         [Route("getDirector")]
         public IHttpActionResult getDirector()
         {
             IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
             return Ok(unitOfWork.UserInfo.GetDirector());
         }
+        /// <summary>
+        /// Students who Completed their Trainings
+        /// </summary>
+        /// <returns></returns>
 
-        //StudentsFinishedTrainings
         [Route("getStudentsFinishedTrainings")]
+
         public IHttpActionResult getStudentsFinishedTrainings()
         {
             IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
@@ -68,7 +89,11 @@ namespace MBAProfile.Controllers
                 return Ok(unitOfWork.StudentsInfo.StudentsFinishedTrainings());
             }
         }
-
+        /// <summary>
+        /// Students who had not completed trainngs
+        /// </summary>
+        /// <returns></returns>
+        /// 
         [Route("getStudentsWithTrainings")]
         public IHttpActionResult getStudentsWithTrainings()
         {
@@ -77,7 +102,10 @@ namespace MBAProfile.Controllers
                 return Ok(unitOfWork.StudentsInfo.StudentsWithACMTrainingDue());
             }
         }
-
+        /// <summary>
+        /// Pulls all the active advisors
+        /// </summary>
+        /// <returns></returns>
         [Route("getActiveAdvisors")]
         public IHttpActionResult getAllActiveAdvisors()
         {
@@ -86,7 +114,10 @@ namespace MBAProfile.Controllers
                 return Ok(unitOfWork.AdvisorInfo.getAllActiveAdvisors());
             }
         }
-
+        /// <summary>
+        /// Pulls all Inactive Advisors
+        /// </summary>
+        /// <returns></returns>
         [Route("getInActiveAdvisors")]
         public IHttpActionResult getAllInActiveAdvisors()
         {
@@ -96,6 +127,10 @@ namespace MBAProfile.Controllers
             }
         }
 
+        /// <summary>
+        /// Fetches all the concentrations available
+        /// </summary>
+        /// <returns></returns>
         [Route("GetPrograms")]
         public IHttpActionResult getAllPrograms()
         {
@@ -103,6 +138,10 @@ namespace MBAProfile.Controllers
             return Ok(unitOfWork.ProgramInfo.getAll());
         }
 
+        /// <summary>
+        /// All the Major details
+        /// </summary>
+        /// <returns></returns>
         [Route("GetMajors")]
         public IHttpActionResult getMajors()
         {
@@ -110,6 +149,10 @@ namespace MBAProfile.Controllers
             return Ok(unitOfWork.MajorsInfo.getAll());
         }
 
+        /// <summary>
+        /// Fetches all the Roles(Advisor,Director and Student)
+        /// </summary>
+        /// <returns></returns>
         [Route("GetRoles")]
         public IHttpActionResult getRoles()
         {
@@ -117,6 +160,10 @@ namespace MBAProfile.Controllers
             return Ok(unitOfWork.RolesInfo.getAll());
         }
 
+        /// <summary>
+        /// Get Students who had not completed the Academic Status
+        /// </summary>
+        /// <returns></returns>
         [Route("GetStudentAcademicStatus")]
         public IHttpActionResult getStudentAcademicStatus()
         {
@@ -124,6 +171,10 @@ namespace MBAProfile.Controllers
             return Ok(unitOfWork.StudentAcademicStatusInfo.getAll());
         }
 
+        /// <summary>
+        /// Get Students with respective to training details
+        /// </summary>
+        /// <returns></returns>
         [Route("GetStudentTrainingStatus")]
         public IHttpActionResult getStudentTrainingStatus()
         {
@@ -131,13 +182,20 @@ namespace MBAProfile.Controllers
             return Ok(unitOfWork.StudentTrainingStatusInfo.getAll());
         }
 
+        /// <summary>
+        /// Get table of Training Statuses
+        /// </summary>
+        /// <returns></returns>
         [Route("GetTrainingRepo")]
         public IHttpActionResult getTrainingRepo()
         {
             IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
             return Ok(unitOfWork.TrainingInfo.getAll());
         }
-
+        /// <summary>
+        /// Pulls out all Courses
+        /// </summary>
+        /// <returns></returns>
         [Route("GetCourses")]
         public IHttpActionResult getCourses()
         {
@@ -146,46 +204,81 @@ namespace MBAProfile.Controllers
         }
 
         #endregion
-
+        /// Creates Student Profile with Details   
         [Route("AddStudent")]
         [HttpPost]
         public IHttpActionResult addStudent([FromBody]UCMStudent student)
         {
             IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
             unitOfWork.StudentsInfo.Add(student);
-            int save = 0;
-            try
-            {
-                save = unitOfWork.Save();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            if (save >= 1)
+            if (unitOfWork.Save() >= 1)
             {
                 return Ok("Success");
             }
-            //if (unitOfWork.Save() >= 1)
-            //{
-            //    return Ok("Success");
-            //}
             return BadRequest("There is an error");
         }
-
+        /// <summary>
+        /// Updates the student details
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns></returns>
         [Route("UpdateStudent")]
         [HttpPost]
         public IHttpActionResult updateStudent([FromBody] UCMStudent student)
         {
+
             IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
             unitOfWork.StudentsInfo.Update(student);
-            if(unitOfWork.Save() >= 1)
+            if (unitOfWork.Save() >= 1)
             {
                 return Ok("Success");
             }
             return BadRequest("There is an error");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns></returns>
+        [Route("addProgram")]
+        [HttpPost]
+        public IHttpActionResult addProgram([FromBody]Program program)
+        {
+            IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
+            unitOfWork.ProgramInfo.Add(program);
+            if (unitOfWork.Save() >= 1)
+            {
+                return Ok("Success");
+            }
+            return BadRequest("There is an error");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns></returns>
+        [Route("UpdateProgram")]
+        [HttpPost]
+        public IHttpActionResult updateProgram([FromBody]Program program)
+        {
+
+            IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
+            unitOfWork.ProgramInfo.Update(program);
+            if (unitOfWork.Save() >= 1)
+            {
+                return Ok("Success");
+            }
+            return BadRequest("There is an error");
+        }
+
+        /// <summary>
+        /// Creates the advisor details.
+        /// </summary>
+        /// <param name="advisor"></param>
+        /// <returns></returns>
+        /// 
         [Route("AddAdvisor")]
         [HttpPost]
         public IHttpActionResult addAdvisor([FromBody] UCMModerator advisor)
@@ -201,19 +294,28 @@ namespace MBAProfile.Controllers
             }
         }
 
+        /// <summary>
+        /// Updates the advisor details
+        /// </summary>
+        /// <param name="advisor"></param>
+        /// <returns></returns>
         [Route("UpdateAdvisor")]
         [HttpPost]
         public IHttpActionResult updateAdvisor([FromBody] UCMModerator advisor)
         {
             IUnitOfWork unitOfWork = new UnitOfWork(new Entities());
             unitOfWork.AdvisorInfo.Update(advisor);
-            if(unitOfWork.Save()>=1)
+            if (unitOfWork.Save() >= 1)
             {
                 return Ok("Success");
             }
             return BadRequest("There is an error");
         }
-
+        /// <summary>
+        /// Can add the Couse details
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         [Route("AddCourse")]
         [HttpPost]
         public IHttpActionResult addCourse([FromBody] CourseInfo course)
@@ -229,6 +331,11 @@ namespace MBAProfile.Controllers
             }
         }
 
+        /// <summary>
+        /// Updates the Course Details
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         [Route("UpdateCourse")]
         [HttpPost]
         public IHttpActionResult updateCourse([FromBody] CourseInfo course)
@@ -244,6 +351,11 @@ namespace MBAProfile.Controllers
             }
         }
 
+        /// <summary>
+        /// Updates the User details
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         [Route("updateUser")]
         [HttpPost]
         public IHttpActionResult updateUser([FromBody] UCMUser user)
